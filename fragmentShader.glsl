d0894aeab5ed91852905761a7d51d7fac46ae9ff
fill-p <83204197+fill-p@users.noreply.github.com>
2022-12-29 16:53:29 +0200

    Add files via upload

---
+++ fragmentShader.glsl
@@ -0,0 +1,15 @@
+const fragmentShaderSource = `
+#ifdef GL_FRAGMENT_PRECISION_HIGH
+    precision highp float;
+#else
+    precision mediump float;
+#endif
+
+varying vec4 color;
+varying vec2 vTextureCoords;
+uniform sampler2D textureU;
+
+void main() {
+    vec4 texture = texture2D(textureU, vTextureCoords);
+    gl_FragColor = texture * color;
+}`;
